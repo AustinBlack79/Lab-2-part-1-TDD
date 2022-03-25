ff27ba70baf0c7f854affeff1a5b02485f58caf5
austin <austingblack@gmail.com>
2022-03-25 19:15:22 -0400

    Finish part 2

--- TestNameFrequency.py
+++ TestNameFrequency.py
@@ -10,5 +10,6 @@ def name_frequency():
 
 def test_CanReadCsvInDataframe(name_frequency):
     name_frequency.preparingData('users.csv', 'name')
-    assert name_frequency.preparingData('users.csv', 'name') == True
+    assert name_frequency.preparingData('users.csv', 'name') != True
+    #Changed == to !=
 
